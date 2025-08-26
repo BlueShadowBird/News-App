bf4b24e339b7ecda44d370d81984e87c0f450dc8
Dede Kurniawan <blue_shadow_bird@yahoo.co.id>
2025-08-26 22:51:04 +0700

    fixing flow timpang tindih

--- app/src/main/java/id/web/dedekurniawan/newsapp/presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/id/web/dedekurniawan/newsapp/presentation/viewmodel/NewsViewModel.kt
@@ -2,36 +2,38 @@ package id.web.dedekurniawan.newsapp.presentation.viewmodel
 
 import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import id.web.dedekurniawan.newsapp.core.Result
 import id.web.dedekurniawan.newsapp.domain.model.News
 import id.web.dedekurniawan.newsapp.domain.usecase.NewsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class NewsViewModel(private val useCase: NewsUseCase): ViewModel() {
-    private val _newsListResult = MediatorLiveData<Result<List<News>>>()
-    val newsListResult: LiveData<Result<List<News>>> = _newsListResult
+    private val queryNews = MutableStateFlow<String?>(null)
 
-    fun retrieveNews(query: String? = null){
-        viewModelScope.launch {
-            _newsListResult.addSource(useCase.retrieveNews(query).asLiveData()){
-                _newsListResult.value = it
-            }
-        }
+    val newsListResult: LiveData<Result<List<News>>> = queryNews
+        .flatMapLatest { useCase.retrieveNews(it) }
+        .asLiveData()
+
+    fun retrieveNews(query: String?) {
+        this.queryNews.value = query
     }
 
-    private val _bookmarkNewsListResult = MediatorLiveData<Result<List<News>>>()
-    val bookmarkNewsListResult: LiveData<Result<List<News>>> = _bookmarkNewsListResult
 
-    fun retrieveBookmarkNews(query: String? = null){
-        viewModelScope.launch {
-            _bookmarkNewsListResult.addSource(useCase.retrieveBookmarkNews(query).asLiveData()){
-                _bookmarkNewsListResult.value = it
-            }
-        }
+    private val queryBookmark = MutableStateFlow<String?>(null)
+
+    val bookmarkNewsListResult: LiveData<Result<List<News>>> = queryBookmark
+        .flatMapLatest { useCase.retrieveBookmarkNews(it) }
+        .asLiveData()
+
+    fun retrieveBookmarkNews(query: String?) {
+        this.queryBookmark.value = query
     }
 
     fun handleBookmarkTrigger(news: News, bitmap: Bitmap? = null) {
@@ -39,4 +41,5 @@ class NewsViewModel(private val useCase: NewsUseCase): ViewModel() {
             useCase.handleBookmarkTrigger(news, bitmap)
         }
     }
+
 }
